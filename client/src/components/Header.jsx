278ba5ee4b34e88157e4a0fd8bdf25f10c80dce2
agrawalom711@gmail.com <agrawalom711@gmail.com>
2024-02-29 15:02:33 +0530

    created server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
 function Header() {
-    const path=useLocation().pathname
+  const path = useLocation().pathname;
   return (
     <Navbar className="border-b-2  ">
       <Link
@@ -33,30 +33,22 @@ function Header() {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-            <Button gradientDuoTone='purpleToBlue'>
-                Sign-In
+          <Button gradientDuoTone="purpleToBlue">Sign-In</Button>
+        </Link>
 
-            </Button>
-            
-            </Link>
-
-            <Navbar.Toggle/>
-
-            
-
+        <Navbar.Toggle />
       </div>
-            <Navbar.Collapse>
-                <Navbar.Link active={path==="/"} as={"div"}>
-                    <Link to="/">Home</Link>
-                </Navbar.Link>
-                <Navbar.Link active={path==="/about"} as={"div"}>
-                    <Link to="/about" >About</Link>
-                </Navbar.Link>
-                <Navbar.Link active={path==="/projects"} as={"div"}>
-                    <Link to="/projects" >Projects</Link>
-                </Navbar.Link>
-            </Navbar.Collapse>
-
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
