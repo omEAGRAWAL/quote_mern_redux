b384113e88b6686d953d0aa011ad221988ee0f53
agrawalom711@gmail.com <agrawalom711@gmail.com>
2024-02-29 12:01:24 +0530

    created the header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import About from './pages/About'
 import Dashboard from './pages/DashBoard'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'  
 function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/about" element={<About />} />
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
+
   theme: {
     extend: {},
+    content: [
+      // ...
+    ],
+    plugins: [
+      // ...
+      import("flowbite/plugin"),
+    ],
   },
-  plugins: [],
 };
